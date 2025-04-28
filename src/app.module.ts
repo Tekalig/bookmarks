eb66fbb3abdd498142c77bd27367a214b98451f8
Tekalig <tekahazi06@gmail.com>
2025-04-28 21:22:37 +0300

    feat: initialize AppModule with necessary imports for Auth, User, Bookmark, Db, and Config modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,19 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { DbModule } from './db/db.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    DbModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
