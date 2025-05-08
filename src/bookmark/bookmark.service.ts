e2065f438cf7f71cff40a29a018224f10d9a8642
Tekalig <tekahazi06@gmail.com>
2025-05-08 23:21:46 +0300

    feat: implement BookmarkService for managing bookmarks

---
+++ src/bookmark/bookmark.service.ts
@@ -0,0 +1,76 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { DbService } from 'src/db/db.service';
+import { CreateBookmarkPayload, EditBookmarkPayload } from './payload';
+
+@Injectable()
+export class BookmarkService {
+  constructor(private db: DbService) {}
+  getBookmarks(userId: number) {
+    return this.db.bookmark.findMany({
+      where: {
+        userId,
+      },
+    });
+  }
+
+  getBookmarkById(userId: number, bookmarkId: number) {
+    return this.db.bookmark.findFirst({
+      where: {
+        id: bookmarkId,
+        userId,
+      },
+    });
+  }
+
+  async createBookmark(userId: number, payload: CreateBookmarkPayload) {
+    const bookmark = await this.db.bookmark.create({
+      data: {
+        userId,
+        ...payload,
+      },
+    });
+
+    return bookmark;
+  }
+
+  async editBookmarkById(
+    userId: number,
+    bookmarkId: number,
+    payload: EditBookmarkPayload,
+  ) {
+    const bookmark = await this.db.bookmark.findUnique({
+      where: {
+        id: bookmarkId,
+      },
+    });
+
+    if (!bookmark || bookmark?.userId !== userId)
+      throw new ForbiddenException('Access to resource is denied');
+
+    return this.db.bookmark.update({
+      where: {
+        id: bookmarkId,
+      },
+      data: {
+        ...payload,
+      },
+    });
+  }
+
+  async deleteBookmarkById(userId: number, bookmarkId: number) {
+    const bookmark = await this.db.bookmark.findUnique({
+      where: {
+        id: bookmarkId,
+      },
+    });
+
+    if (!bookmark || bookmark?.userId !== userId)
+      throw new ForbiddenException('Access to resource is denied');
+
+    return this.db.bookmark.delete({
+      where: {
+        id: bookmarkId,
+      },
+    });
+  }
+}
